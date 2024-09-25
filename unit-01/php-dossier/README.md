c025f63f033f8a8610c3f15b99f3904e2e808de3
Nabil L.A. <nabil14716@gmail.com>
2024-09-25 13:33:02 +0100

    feat(practice33): Finished practice33
    Closes #35

--- unit-01/php-dossier/README.md
+++ unit-01/php-dossier/README.md
@@ -1433,15 +1433,26 @@ Tomar captura de pantalla
 
 ```
 <?php
+    $conUrlEncode = urlencode('Pasando datos diría.. que hay que usar urlencode');
+    echo "<a href=practice33.php?prueba='Pasando datos diría.. que hay que usar urlencode'>pasando datos</a>";
+    echo "<br></br>";
+    echo "<a href=practice33.php?prueba2={$conUrlEncode}>pasando datos</a>";
 
-
+    $recibido = $_GET["prueba"] ?? "nadita";
+    $recibidoConEncode = $_GET["prueba2"] ?? "nadita";
+    echo "<h3>se ha recibido:</h3>";
+    echo "prueba: ". $recibido . "<br>";
+    echo "prueba: ". $recibidoConEncode . "<br>";
 ?>
+
 ```
 
 - Captura:
 
 <div align="center">
-<img src="./img/p33.png"/>
+<img src="./img/p33-1.png"/>
+<img src="./img/p33-2.png"/>
+<img src="./img/p33-3.png"/>
 </div>
 
 ***
--- unit-01/php-dossier/practices/practice33.php
+++ unit-01/php-dossier/practices/practice33.php
@@ -1,26 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
 <?php
-    $conUrlEncode = urlencode('Pasando datos diría.. que hay que usar urlencode');
-    
-    echo "<a href=practica33.php?prueba='Pasando datos diría.. que hay que usar urlencode'>pasando datos sin utlencode</a>";
-    echo "</br>";
-    echo "<a href=practica33.php?prueba2={$conUrlEncode}>pasando datos sin utlencode</a>";
+$conUrlEncode = urlencode('Pasando datos diría.. que hay que usar urlencode');
+echo "<a href=practice33.php?prueba='Pasando datos diría.. que hay que usar urlencode'>pasando datos</a>";
+echo "<br></br>";
+echo "<a href=practice33.php?prueba2={$conUrlEncode}>pasando datos</a>";
 
-
-    $recibidoSinUrlEncode = $_GET["prueba"] ?? "nadita";
-    $recibidoConUrlEncode = $_GET["prueba2"] ?? "nadita";
-    
-    echo "<h3>se ha recibido:</h3>";
-    echo "prueba: ". $recibidoUrlEncode . "<br>";
-    echo "prueba2: ". $recibidoSinUrlEncode . "<br>";
+$recibido = $_GET["prueba"] ?? "nadita";
+$recibidoConEncode = $_GET["prueba2"] ?? "nadita";
+echo "<h3>se ha recibido:</h3>";
+echo "prueba: ". $recibido . "<br>";
+echo "prueba: ". $recibidoConEncode . "<br>";
 ?>
-</body>
-</html>

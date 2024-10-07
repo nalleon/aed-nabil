5d5bcfa1d3e8f48914b82a17b6f0ac2c7ac141f0
Nabil L.A. <nabil14716@gmail.com>
2024-10-07 23:04:50 +0100

    feat(blackjack): Definied Player's attributes, constructor, get/set

--- exam-activities/blackjack/app/Models/Player.php
+++ exam-activities/blackjack/app/Models/Player.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Player extends Model
 {
     use HasFactory;
+    /**
+     * @var string name of the player
+     */
+    private $playerName;
+    /**
+     * @var array of cards in the player's hand
+     */
+    private $hand = []; 
+    /**
+     * @var int number of points of the player
+     */
+    private $score; 
+
+
+    /**
+     * Constructor of the class
+     */
+    public function __construct($playerName) {
+        $this->playerName = $playerName;
+        $this->score = 0;
+    }
+
+
 }

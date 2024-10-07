f0a64a0a0fae12c76ee8a3f7900f84d8b82eab71
Nabil L.A. <nabil14716@gmail.com>
2024-10-07 22:32:34 +0100

    feat(blackjack): Created basic structure of the project

---
+++ exam-activities/blackjack/app/Models/DeckCards.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DeckCards extends Model
+{
+    use HasFactory;
+}

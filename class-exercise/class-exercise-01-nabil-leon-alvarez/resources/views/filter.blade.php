bb2280540a835824054d8ac2af3a80965490a122
Nabil L.A. <nabil14716@gmail.com>
2024-10-07 12:57:11 +0100

    feat(class-exercise-01): Created filter.blade.php

---
+++ class-exercise/class-exercise-01-nabil-leon-alvarez/resources/views/filter.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Nabil León Álavarez">
+        <title>Class exercise</title>
+        <link rel="stylesheet" href="./style/startpage.css">
+
+    </head>
+    <body class="antialiased">
+        <div class="main-container">
+            
+        </div>
+    </body>
+</html>

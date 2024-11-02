069a1eab634ba3dfa08638078214806c93a7b8aa
Nabil L.A. <nabil14716@gmail.com>
2024-11-02 14:43:03 +0000

    style(construction-game): Added bootstrap css to register page

--- exam-activities/construction-game/resources/views/login.blade.php
+++ exam-activities/construction-game/resources/views/login.blade.php
@@ -9,28 +9,23 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    </head>
-    <header>
-        <nav class="navbar navbar-dark bg-dark text-light navbar-expand-lg mb-5 ">
-          <div class="container-fluid">
-              <h4 class="mt-2 me-2 ms-5"><i class="ms-3 bi bi-buildings-fill text-light"></i> Construction Game </h4>
-            
-    
-          </div>
-
- 
-        </nav>
-    </header>
-      <style>
-        .custom-shadow {
-            border: 3px solid rgba(28, 63, 132, 0.2);
-            box-shadow: 4px 8px 12px rgba(28, 63, 132, 0.2);
-        }  
-    </style>
+        <header>
+            <nav class="navbar navbar-dark bg-dark text-light navbar-expand-lg mb-5 ">
+            <div class="container-fluid">
+                <h4 class="mt-2 me-2 ms-5"><i class="ms-3 bi bi-buildings-fill text-light"></i> Construction Game </h4>
+            </div>
+            </nav>
+        </header>
+        <style>
+            .custom-shadow {
+                border: 3px solid rgba(28, 63, 132, 0.2);
+                box-shadow: 4px 8px 12px rgba(28, 63, 132, 0.2);
+            }  
+        </style>
     </head>
     <body class="antialiased">
         <div class="container d-flex justify-content-center text-light mt-5 mb-5">
-            <div class="card bg-dark custom-shadow text-light rounded mt-3 position-relative w-100  col-6">
+            <div class="card bg-dark custom-shadow text-light rounded mt-3 position-relative w-75 col-10 col-md-6 col-lg-4"  >
                 <div class="card-header bg-light text-dark">
                     <h3 class="text-center m-3">Login</h3>
                 </div>

f65ee4570d4338447a744532497f1de4c67dffe8
Nabil L.A. <nabil14716@gmail.com>
2024-12-15 18:06:17 +0000

    feat(api-movies): Added jwt dependencias and fixed issue

--- exam-activities/api-movies/app/Http/Controllers/Controller.php
+++ exam-activities/api-movies/app/Http/Controllers/Controller.php
@@ -1,7 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+/**
+* @OA\Info(
+* title="Instituto api",
+* version="1.0.0",
+* description="Esta es la documentación de la API generada automáticamente con Swagger",
+* @OA\Contact(
+* email="soporte@example.com"
+* )
+* )
+*/
 abstract class Controller
 {
     //

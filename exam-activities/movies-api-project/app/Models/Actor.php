2f7f0038fc713a488268a61475febec116f066db
Nabil L.A. <nabil14716@gmail.com>
2024-12-16 17:41:30 +0000

    feat(movies-api-project): Generated models for tables

---
+++ exam-activities/movies-api-project/app/Models/Actor.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property string $nombre
+ * @property string $apellidos
+ * @property Pelicula[] $peliculas
+ */
+class Actor extends Model
+{
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'actor';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['nombre', 'apellidos'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function peliculas()
+    {
+        return $this->belongsToMany('App\Models\Pelicula', 'pelicula_actor');
+    }
+}

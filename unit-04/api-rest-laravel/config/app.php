5ad3489e3b44072168a541f780461678245282d7
Nabil L.A. <nabil14716@gmail.com>
2024-12-17 12:36:08 +0000

    feat(dossier-api): Finished practice06

--- unit-04/api-rest-laravel/config/app.php
+++ unit-04/api-rest-laravel/config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
     ])->toArray(),
 
     /*

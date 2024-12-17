5e99cffdc419354628d7d9fb05cad8aca7636ec6
Nabil L.A. <nabil14716@gmail.com>
2024-12-17 12:00:07 +0000

    feat(movies-api-project): Added detatch tables in delete of movie

--- exam-activities/movies-api-project/app/Http/Controllers/MovieRESTController.php
+++ exam-activities/movies-api-project/app/Http/Controllers/MovieRESTController.php
@@ -85,6 +85,10 @@ class MovieRESTController extends Controller
      */
     public function destroy(Movie $movie)
     {
+        $movie->categorias()->detach();
+        $movie->actors()->detach();
+        $movie->directors()->detach();
+
         $movie->delete();
         return response()->json(null, 204);
     }

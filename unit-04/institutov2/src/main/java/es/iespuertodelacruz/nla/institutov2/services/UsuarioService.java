075f5adfe33668b6b9f967488c42154f27405214
nalleon <nabil14716@gmail.com>
2025-01-27 11:22:34 +0000

    feat(institutov2): Created UsuarioService

---
+++ unit-04/institutov2/src/main/java/es/iespuertodelacruz/nla/institutov2/services/UsuarioService.java
@@ -0,0 +1,48 @@
+package es.iespuertodelacruz.nla.institutov2.services;
+
+import es.iespuertodelacruz.nla.institutov2.entities.Asignatura;
+import es.iespuertodelacruz.nla.institutov2.entities.Usuario;
+import es.iespuertodelacruz.nla.institutov2.repository.IUsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Service
+public class UsuarioService implements IServiceGeneric<Usuario, String>{
+    @Autowired
+    IUsuarioRepository repository;
+    @Override
+    public List<Usuario> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Usuario findById(String id) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Usuario save(Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public boolean delete(String id) {
+        try {
+            int quantity = repository.deleteUsuarioByDNI(id);
+            return quantity > 0;
+        } catch (RuntimeException e){
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    @Transactional
+    public boolean update(Usuario usuario) {
+
+        return false;
+    }
+}

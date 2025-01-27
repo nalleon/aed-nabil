2ba6da02f6a64a23897df934156bbcffa5081dc3
nalleon <nabil14716@gmail.com>
2025-01-27 11:17:49 +0000

    feat(institutov2): Added custom querys to UserRepository

--- unit-04/institutov2/src/main/java/es/iespuertodelacruz/nla/institutov2/repository/IUsuarioRepository.java
+++ unit-04/institutov2/src/main/java/es/iespuertodelacruz/nla/institutov2/repository/IUsuarioRepository.java
@@ -2,8 +2,31 @@ package es.iespuertodelacruz.nla.institutov2.repository;
 
 import es.iespuertodelacruz.nla.institutov2.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IUsuarioRepository extends JpaRepository<Usuario, String> {
 
 
+    @Modifying
+    @Query(
+            value="DELETE FROM usuarios AS u WHERE u.dni=:dni",
+            nativeQuery=true
+    )
+    int deleteUsuarioByDNI(@Param("dni") String dni);
+
+
+    @Query(
+            value="SELECT * FROM usuarios WHERE nombre LIKE %:nombre%",
+            nativeQuery=true
+    )
+    Usuario findUsuarioByNombre(@Param("nombre") String nombre);
+
+
+    @Query(
+            value="SELECT * FROM usuarios WHERE correo LIKE %:correo%",
+            nativeQuery=true
+    )
+    Usuario findUsuarioByCorreo(@Param("correo") String correo);
 }

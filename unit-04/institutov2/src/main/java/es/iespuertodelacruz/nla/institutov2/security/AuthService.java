173463d1d5e524ff70cbe152aab490ed5bfbb662
nalleon <nabil14716@gmail.com>
2025-01-27 12:27:26 +0000

    feat(instituto-app): Implemented AuthServive, JwtFilter, JwtService and SecurityConfig

--- unit-04/institutov2/src/main/java/es/iespuertodelacruz/nla/institutov2/security/AuthService.java
+++ unit-04/institutov2/src/main/java/es/iespuertodelacruz/nla/institutov2/security/AuthService.java
@@ -1,4 +1,70 @@
 package es.iespuertodelacruz.nla.institutov2.security;
+import es.iespuertodelacruz.nla.institutov2.entities.Usuario;
+import es.iespuertodelacruz.nla.institutov2.repository.IUsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class AuthService {
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    public String register(String username, String password, String email) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre(username);
+        usuario.setPassword(passwordEncoder.encode(password));
+        usuario.setCorreo(email);
+        usuario.setRol("ROLE_USER");
+
+        Usuario saved = usuarioRepository.save(usuario);
+
+        if( saved != null) {
+            String generateToken = jwtService.generateToken(usuario.getNombre(), usuario.getRol());
+            return generateToken;
+        }else {
+            return null;
+        }
+    }
+
+
+
+    public String register(String username, String password) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre(username);
+        usuario.setPassword(passwordEncoder.encode(password));
+        usuario.setRol("ROLE_USER");
+
+        Usuario saved = usuarioRepository.save(usuario);
+
+        if( saved != null) {
+            String generateToken = jwtService.generateToken(usuario.getNombre(), usuario.getRol());
+            return generateToken;
+        }else {
+            return null;
+        }
+    }
+
+    public String authenticate(String username, String password)  {
+        String generateToken = null;
+        Usuario usuario = usuarioRepository.findUsuarioByNombre(username).orElse(null);
+
+
+        if (usuario != null) {
+            if (passwordEncoder.matches(password, usuario.getPassword())) {
+                generateToken = jwtService.generateToken(usuario.getNombre(), usuario.getRol());
+            }
+        }
+
+
+        return generateToken;
+    }
 }
--- unit-04/institutov2/src/main/java/es/iespuertodelacruz/nla/institutov2/security/SecurityConfig.java
+++ unit-04/institutov2/src/main/java/es/iespuertodelacruz/nla/institutov2/security/SecurityConfig.java
@@ -11,6 +11,44 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+
+
+    @Autowired	private JwtFilter jwtAuthFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(cors->cors.disable())
+                .csrf(csrf -> csrf.disable() )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+
+                        .requestMatchers(
+                                "/", "/swagger-ui.html",
+                                "/swagger-ui/**", "/v2/**",
+                                "/configuration/**",	"/swagger*/**",
+                                "/webjars/**", "/api/login",
+                                "/api/register", "/v3/**",
+                                "/websocket*/**", "/index.html", "/api/v1/**"
+                        ).permitAll()
+
+                        .requestMatchers("/api/v3/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.getOrBuild();
+    }
 }

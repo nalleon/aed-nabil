96e0dada629d1b58e5015f04ad45686f6b539b64
nalleon <nabil14716@gmail.com>
2025-01-27 12:55:18 +0000

    feat(instituto-app): Implemented methods in UsuarioControllerV3

--- unit-04/institutov2/src/main/java/es/iespuertodelacruz/nla/institutov2/controller/UsuarioControllerV3.java
+++ unit-04/institutov2/src/main/java/es/iespuertodelacruz/nla/institutov2/controller/UsuarioControllerV3.java
@@ -2,6 +2,7 @@ package es.iespuertodelacruz.nla.institutov2.controller;
 
 import java.util.List;
 
+import es.iespuertodelacruz.nla.institutov2.controller.interfaces.IController;
 import es.iespuertodelacruz.nla.institutov2.entities.Usuario;
 import es.iespuertodelacruz.nla.institutov2.repository.IUsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,26 +20,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 @RequestMapping("/api/v3/usuarios")
-public class UsuarioControllerV3 {
+public class UsuarioControllerV3 implements IController<Usuario, String> {
     @Autowired
     private IUsuarioRepository usuarioRepository;
 
-    @GetMapping
-    @PreAuthorize("hasRol('ROLE_ADMIN')")
-    public List<Usuario> listarUsuarios() {
-        return usuarioRepository.findAll();
-    }
 
+    @Override
     @PostMapping
     @PreAuthorize("hasRol('ROLE_ADMIN')")
-    public Usuario crearUsuario(@RequestBody Usuario usuario) {
-        return usuarioRepository.save(usuario);
+    public ResponseEntity<?> add(Usuario usuario) {
+        return ResponseEntity.ok(usuarioRepository.save(usuario));
     }
 
+    @Override
+    public ResponseEntity<?> update(String id, Usuario usuario) {
+        return ResponseEntity.ok(usuarioRepository.save(usuario));
+    }
+
+    @Override
+    @GetMapping
+    @PreAuthorize("hasRol('ROLE_ADMIN')")
+    public ResponseEntity<List<Usuario>> getAll() {
+        return ResponseEntity.ok(usuarioRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRol('ROLE_ADMIN')")
+    @Override
+    public ResponseEntity<Usuario> getById(String id) {
+        Usuario usuario = usuarioRepository.findById(id).orElse(null);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @Override
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRol('ROLE_ADMIN')")
-    public ResponseEntity<?> eliminarUsuario(@PathVariable String id) {
-        usuarioRepository.deleteById(id);
+    public ResponseEntity<?> delete(String id) {
+        usuarioRepository.deleteUsuarioByDNI(id);
         return ResponseEntity.noContent().build();
     }
 }
